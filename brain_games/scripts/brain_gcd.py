5aff8aa889a8004c843c4fdba0548446cc64844e
[Buchik] <[yaBuchik@yandex.ru]>
2026-01-22 16:25:30 +0300

    made gcd game

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,23 @@
+import random
+
+from brain_games import logic, welcome_user1
+
+
+def main():
+    name = welcome_user1()
+    rounds = 3
+    for round in range(rounds):
+        x = random.randint(0, 100)
+        y = random.randint(0, 100)
+        task = f"{x} {y}"
+        while y != 0:
+            temp = x
+            x = y
+            y = temp % x
+        correct_ans = str(x)
+        logic(task, correct_ans, name)
+    print(f"Congratulations, {name}!")
+
+
+if __name__ == "__main__":
+    main()
