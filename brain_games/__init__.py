aa92b18b984bf0ee6150c090984e62d02f2e0ef5
[Buchik] <[yaBuchik@yandex.ru]>
2026-01-22 11:56:24 +0300

    fixed linting

--- brain_games/__init__.py
+++ brain_games/__init__.py
@@ -1,3 +1,3 @@
-from .cli import welcome_user
-from .welcome import welcome_user1
+from .Awelcome import welcome_user1
 from .brain_even_logic import even
+from .cli import welcome_user
--- brain_games/brain_even_logic.py
+++ brain_games/brain_even_logic.py
@@ -1,26 +1,30 @@
-from brain_games import welcome_user1
 import random
+
 import prompt
+
+from brain_games import welcome_user1
 
 
 def even():
     name = welcome_user1()
     print('Answer "yes" if the number is even, otherwise answer "no".')
     n = 0
-    while n < 3:
+    num_of_correct_answers = 3
+    while n < num_of_correct_answers:
         number = random.randint(1, 1000)
         print(f"Question: {number}")
-        answer = prompt.string("Your answer: ")
-        if number % 2 == 0 and answer == "yes" or number % 2 != 0 and answer == "no":
+        ans = prompt.string("Your answer: ")
+        if (number % 2 == 0 and ans == "yes" 
+            or number % 2 != 0 and ans == "no"):
             print("Correct!")
             n += 1
         else:
             n = 0
             if number % 2 == 0:
-                print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'." \
+                print(f"'{ans}' is wrong answer ;(. Correct answer was 'yes'."
                 f"Let's try again, {name}!")
             else:
-                print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'." \
+                print(f"'{ans}' is wrong answer ;(. Correct answer was 'no'."
                 f"Let's try again, {name}!")
     print(f"Congratulations, {name}!")
     
