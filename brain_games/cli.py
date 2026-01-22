2a361c2df7646be98affa3fa57879610c790fcac
[Buchik] <[yaBuchik@yandex.ru]>
2026-01-22 17:35:40 +0300

    added space in the end of asking for name

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,5 +4,5 @@ import prompt
 def welcome_user():
 
     print("Welcome to the Brain Games!")
-    name = prompt.string("May i have your name?")
+    name = prompt.string("May i have your name? ")
     print(f"Hello, {name}!")
